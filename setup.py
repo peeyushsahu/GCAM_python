cf8114f7d90c056b41e2a176bc188fba48b64e98
peeyushsahu <peeyushsahu@yahoo.com>
2015-06-02 17:53:38 +0200

    Created setup.py, MANIFEST.in and other files for python package functionality.

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""Description
+
+Setup script for GCAM -- Gene Celltype Association Miner
+
+Copyright (c) 2015 Peeyush Sahu <peeyush.sahu@imt.uni-marburg.de>
+
+This code is free software; you can redistribute it and/or modify it
+under the terms of the BSD License (see the file COPYING included with
+the distribution).
+
+@status:  beta
+@version: $Revision$
+@author:  Peeyush Sahu
+@contact: peeyush.sahu@imt.uni-marburg.de
+"""
+
+import os
+import sys
+from setuptools import setup, Extension
+
+
+def main():
+    if float(sys.version[:3])<2.7 or float(sys.version[:3])>=2.8:
+        sys.stderr.write("CRITICAL: Python version must be 2.7!\n")
+        sys.exit(1)
+
+    setup(name="GCAM",
+          version="1.0.0",
+          description="Gene Celltype Association Miner",
+          author='Peeyush Sahu',
+          author_email='peeyush.sahu@imt.uni-marburg.de',
+          url='https://github.com/peeyushsahu/GCAM_python.git',
+          package_dir={'GCAM' : 'PubmedSearch'},
+          packages=['PubmedSearch'],
+          package_data={'PubmedSearch': ['resources/*']},
+          scripts=['bin/GCAM'],
+
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: Console',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: MacOS :: Linux',
+              'Operating System :: POSIX',
+              'Topic :: Scientific/Engineering :: Bio-Informatics',
+              'Programming Language :: Python',
+          ],
+
+          )
+
+if __name__ == '__main__':
+    main()
+
